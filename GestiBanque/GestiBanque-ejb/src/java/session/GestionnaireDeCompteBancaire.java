7ce206bec75359d3d58e110c2ece9c74d96b38c5
Cecile <cemelay@gmail.com>
2017-10-12 16:09:04 +0200

    session GestionnaireDeCompteBancaire + entity CompteBancaire

---
+++ GestiBanque/GestiBanque-ejb/src/java/session/GestionnaireDeCompteBancaire.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import entity.CompteBancaire;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Utilisateur
+ */
+@Stateless
+@LocalBean
+public class GestionnaireDeCompteBancaire {
+    
+    public void creerComptesTest() {  
+   creerCompte(new CompteBancaire("John Lennon", 150000));  
+   creerCompte(new CompteBancaire("Paul McCartney", 950000));  
+   creerCompte(new CompteBancaire("Ringo Starr", 20000));  
+   creerCompte(new CompteBancaire("Georges Harrisson", 100000));  
+}  
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    private void creerCompte(CompteBancaire compteBancaire) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        return null;
+    }
+}
